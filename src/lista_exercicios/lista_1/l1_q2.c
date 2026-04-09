4a741a8d3c23a2287a85ccaafe47a56f595173e5
Renan Costa Alencar <renan.costaalencar@gmail.com>
2026-04-08 22:04:23 -0300

    feat: add solutions for exercise 2 of the first list demonstrating printf syntax and operator usage

--- src/lista_exercicios/lista_1/l1_q2.c
+++ src/lista_exercicios/lista_1/l1_q2.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 
 /**
- * Problemas da linha de comando printf {"%d", ++(xx + y));
+ * Problemas da linha de comando printf {"%d", (xx + y));
  * 1. '{' em vez de '(' depois de printf;
  * 2. Variável 'x' duplicada 'xx';
  * 3. O pré-incremento só pode acontecer a um valor modificável. A expressão (x + y) não é modificável;
  */
 
 int main() {
-    int x = 3, y = 2;
+    int x = 3, xx= 3, y = 2;
     // Solução 1
     printf ("%d", (x + y));
     // Solução 2
     printf ("\n%d", (x*x + y));
     // Solução 3
-    printf ("\n%d", (x*x + y));
+    printf ("\n%d", (x*x + y) + 1);
+    // Solução 4
+    printf ("\n%d", (xx + y) + 1);
+    // Solução 5
+    printf ("\n%d", (xx + y));
     return 0;
 }
-
-// SAíDA: 5
